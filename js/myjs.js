28cfe17b739de1020bb8e62b33e44e7922de6d75
lBetterManl <yhc8023tm@foxmail.com>
2016-11-18 20:03:34 +0800

    半成版

--- js/myjs.js
+++ js/myjs.js
@@ -1,11 +1,36 @@
 $(function() {
+
+    //导航背景
     $(window).scroll(function(){
-        if($(window).scrollTop() > 5){
+        if($(window).scrollTop() > 10){
         	$("nav").css("background","#eee");
         }
-        if($(window).scrollTop() < 5){
+        if($(window).scrollTop() < 10){
         	$("nav").css("background","none");
         }
 
+    });
+
+    //导航变化
+    $("ul#navList>li").click(function(){
+        var index = $("ul#navList>li").index(this);
+        $("ul#navList>li.active").removeClass("active");
+        if(index!=0){
+            $(this).addClass('active');
+        }
+    });
+
+    //监听关闭model
+    $("#myModal1").on("hidden.bs.modal",function(){
+        $("#player1").Stop();
+    });
+    $("#myModal2").on("hidden.bs.modal",function(){
+        $("#player2").Stop();
+    });
+    $("#myModal3").on("hidden.bs.modal",function(){
+        $("#player3").Stop();
+    });
+    $("#myModal4").on("hidden.bs.modal",function(){
+        $("#player4").Stop();
     });
 });
