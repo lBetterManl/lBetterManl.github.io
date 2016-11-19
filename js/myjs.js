9c525fbc13dda53ece53e67ebea8af450a663ac5
lBetterManl <yhc8023tm@foxmail.com>
2016-11-19 12:16:03 +0800

    优化2

--- js/myjs.js
+++ js/myjs.js
@@ -2,17 +2,17 @@ $(function() {
 
     //导航背景
     $(window).scroll(function(){
-        //console.log($(window).scrollTop());
-        if($(window).scrollTop() <= 150){
+        console.log($(window).scrollTop());
+        if($(window).scrollTop() <= 10){
         	$("nav").css({"background":"none","box-shadow":"none"});
             $("ul#navList>li.active").removeClass("active");
             $(".coverDiv").slideDown();
         }
-        if($(window).scrollTop() > 150){
+        if($(window).scrollTop() > 100){
             $("nav").css({"background":"#eee","box-shadow":"0 1px 6px #777"});
             $(".coverDiv").slideUp();
         }
-        if($(window).scrollTop() >= 450){
+        if($(window).scrollTop() >= 165){
             $("ul#navList>li.active").removeClass("active");
             $("ul#navList>li").eq(1).addClass('active');
         }
@@ -31,7 +31,7 @@ $(function() {
         if(index == 1){
             //滚动到指定位置
             $("html,body").stop(true); 
-            $("html,body").animate({scrollTop:$("#movieLine").offset().top-80},800);
+            $("html,body").animate({scrollTop:210},800);
             $(".imgDiv").animate({opacity:'0.4'},"slow");
             $(".imgDiv").animate({opacity:'0.8'},"slow");
         }
