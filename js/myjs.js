cf884756e3e3147065f7c46b4cbd6426141393e0
lBetterManl <yhc8023tm@foxmail.com>
2016-11-19 10:44:15 +0800

    半完成版本

--- js/myjs.js
+++ js/myjs.js
@@ -4,20 +4,31 @@ $(function() {
     $(window).scroll(function(){
         if($(window).scrollTop() > 10){
         	$("nav").css("background","#eee");
+            $(".coverDiv").slideUp();
         }
-        if($(window).scrollTop() < 10){
+        if($(window).scrollTop() <= 10){
         	$("nav").css("background","none");
+            $("ul#navList>li.active").removeClass("active");
+            $(".coverDiv").slideDown();
         }
 
     });
 
     //导航变化
     $("ul#navList>li").click(function(){
+        var obj = $(this);
         var index = $("ul#navList>li").index(this);
         $("ul#navList>li.active").removeClass("active");
-        if(index!=0){
-            $(this).addClass('active');
+        if(index == 1){
+            obj.addClass('active');
+            //滚动到指定位置
+            $("html,body").stop(true); 
+            $("html,body").animate({scrollTop:$("#movieLine").offset().top-80},600);
         }
+        if(index == 2){
+            obj.addClass('active');
+        }
+
     });
 
     //监听关闭model
@@ -33,4 +44,6 @@ $(function() {
     $("#myModal4").on("hidden.bs.modal",function(){
         $("#player4").trigger('pause');
     });
+
+
 });
